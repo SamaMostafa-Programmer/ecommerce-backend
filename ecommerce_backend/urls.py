ac7852b0a5dfa1b97bab1eb39848714b9c5884f8
Sama Mostafa <samam6778@gmail.com>
2026-02-11 04:00:59 +0200

    Prepare for deployment

--- ecommerce_backend/urls.py
+++ ecommerce_backend/urls.py
@@ -1,31 +1,9 @@
-"""
-URL configuration for ecommerce_backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-from rest_framework import permissions
-
-# urls.py
 
 # urls.py
 schema_view = get_schema_view(
@@ -38,13 +16,12 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
 )
 
-# ضيفي السطرين دول تحت الـ schema_view علطول وقبل الـ urlpatterns
 schema_view.security_definitions = {
     'Bearer': {
         'type': 'apiKey',
         'name': 'Authorization',
         'in': 'header',
-        'description': 'اكتبي كلمة Bearer وبعدها مسافة ثم التوكن'
+        'description': 'Bearer + ""'
     }
 }
 
@@ -53,10 +30,7 @@ schema_view.security = [{'Bearer': []}]
 # Swagger UI URLs
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
-    # السطر ده هو اللي هيحل مشكلة الـ 404 لما تدوسي Django Login
     path('api-auth/', include('rest_framework.urls')), 
-
     path('api/', include('store.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
